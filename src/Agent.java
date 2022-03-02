4f6706f5f17cbfcb261900c3b7c07e9b6da40c8d
peterrevsbech <peter.revsbech@gmail.com>
2022-03-02 10:59:58 +0100

    Remade some code that was lost (maybe in merge)

--- src/Agent.java
+++ src/Agent.java
@@ -1,17 +1,33 @@
 import java.util.ArrayList;
 
 public class Agent implements Player{
+    ArrayList<Integer> moveSequence;
+    boolean hasNextMoveReady = false;
+    int moveSeqIndex = 0;
 
     public int getMove(State state) {
-        return 0;
+        if (!hasNextMoveReady){
+            calculateMoveSeq();
+            if (moveSequence.size()==0){
+                System.out.println("ERROR - Agent was asked for next move, but none were possible");
+            }
+            hasNextMoveReady = true;
+            moveSeqIndex = 0;
+        }
+        int result = moveSequence.get(moveSeqIndex);
+        moveSeqIndex++;
+        if (moveSeqIndex == moveSequence.size()){
+            hasNextMoveReady = false;
+        }
+
+        return result;
     }
 
-    public ArrayList<Integer> getMoveSeq(){
+    public void calculateMoveSeq(){
         ArrayList<Integer> moveSeq = new ArrayList<Integer>();
         //Make each index be the move it chooses at a given node.
         //This way we may move all the way through our composite node.
-        
-        return moveSeq;
+        moveSequence = moveSeq;
     }
 
     public int minMax(State state){
