300a96c68b7ab45f53c18f5e9dfbb2bdff6faa06
s194579 <59560997+s194579@users.noreply.github.com>
2022-03-02 10:19:09 +0100

    Update Agent.java

--- src/Agent.java
+++ src/Agent.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class Agent implements Player{
 
     public int getMove(State state) {
         return 0;
     }
+
+    public ArrayList<Integer> getMoveSeq(){
+        ArrayList<Integer> moveSeq = new ArrayList<Integer>();
+        //Make each index be the move it chooses at a given node.
+        //This way we may move all the way through our composite node.
+        
+        return moveSeq;
+    }
+
+    public int minMax(State state){
+        return 0;
+    }
+
 }
