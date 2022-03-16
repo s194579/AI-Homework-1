6d01eed2d5a65ac7ac99dfee8415624792c718dc
peterrevsbech <peter.revsbech@gmail.com>
2022-03-16 10:03:56 +0100

    Added small test of Ai vs AI

--- src/Application.java
+++ src/Application.java
@@ -11,6 +11,19 @@ public class Application {
         GameController gc = new GameController();
         gc.p1 = new Agent(true,50);
         gc.p2 = new Agent(false,50);
-        gc.start(12);
+        gc.start(4);
+    }
+
+    public static void performAgentVsAgentTest(){
+        GameController gc = new GameController();
+        gc.p1 = new Agent(true,50);
+        gc.p2 = new Agent(false,50);
+        int testSize = 10;
+        for (int i = 0; i < testSize; i++) {
+            gc.start(4);
+        }
+        System.out.println();
+        System.out.println("P1 won: " + gc.p1Wins + " and P2 won " + gc.p2Wins + " out of " + testSize +" games");
+
     }
 }
--- src/GameController.java
+++ src/GameController.java
@@ -3,9 +3,11 @@ public class GameController {
     private State gameState;
     public Player p1, p2;
     static int turn = 0;
-
+    public double p1Wins = 0;
+    public double p2Wins = 0;
 
     public void start(int initalSeedsPrHouse){
+        turn = 0;
         game = new Game();
         gameState = game.initialSetUp(initalSeedsPrHouse);
         while (!game.goalTest(gameState)){
@@ -22,5 +24,19 @@ public class GameController {
         }
         UIController.printState(gameState, turn);
         UIController.printWinner(gameState.getP1PointsFinishedGame(), gameState.getP2PointsFinishedGame());
+        assignPoints();
+    }
+
+    private void assignPoints(){
+        int pointDiff = gameState.getPointDiffFinishedGame();
+        if (pointDiff == 0){
+            p1Wins += 0.5;
+            p2Wins += 0.5;
+        } else if (pointDiff > 0){
+            p1Wins += 1;
+        } else {
+            p2Wins += 1;
+        }
+
     }
 }
