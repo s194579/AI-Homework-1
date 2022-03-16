334b2632cf1ff28b5cb3db9a8697e3391831d64b
MrGundulf <48431504+MrGundulf@users.noreply.github.com>
2022-03-16 09:48:59 +0100

    Made interface for starting game

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -7,10 +8,36 @@ public class Application {
         //Create Agent
         //Create Game Controller
         //Create UI controller
+        int searchTimeMillis = 0;
+        boolean isPlayerAI = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Player1:");
+        String player1 = scanner.nextLine();
+
+        System.out.println("Player2:");
+        String player2 = scanner.nextLine();
+
+        System.out.println("Number of seeds in each house:");
+        int initialSeedsPrHouse = scanner.nextInt();
+
+        if(player1.equals("AI") || player2.equals("AI")) {
+            System.out.println("Time the AI has on each search layer:");
+            searchTimeMillis = scanner.nextInt();
+        }
 
         GameController gc = new GameController();
-        gc.p1 = new Agent(true,50);
-        gc.p2 = new Agent(false,50);
-        gc.start(12);
+
+        if (player1.equals("AI")) {
+            gc.p1 = new Agent(true, searchTimeMillis);
+        }else {
+            gc.p1 = new UIPlayer();
+        }
+        if (player2.equals("AI")) {
+            gc.p2 = new Agent(false, searchTimeMillis);
+        }else {
+            gc.p2 = new UIPlayer();
+        }
+
+        gc.start(initialSeedsPrHouse);
     }
 }
